3207bde7f4e74a848eac41803844967903977276
RAHUL KUMAR <srrahul092@gmail.com>
2025-02-14 12:19:12 +0530

    tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <main className="text-red-500 text-3xl font-bold">
+
+      Hello world!
+
+    </main>
+  )
+}
+
+export default App
