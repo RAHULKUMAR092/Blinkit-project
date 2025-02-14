cdc40d2277192e0c9992ed45ad68b9affe5f94c5
RAHUL KUMAR <srrahul092@gmail.com>
2025-02-14 12:43:19 +0530

    home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <main className="text-red-500 text-3xl font-bold">
-
-      Hello world!
+    <main>
+      <Outlet />
 
     </main>
   )
