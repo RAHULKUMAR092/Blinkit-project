67a32f73d730d4b8e1a1f815da182f5d81f0322c
RAHUL KUMAR <srrahul092@gmail.com>
2025-02-15 21:06:12 +0530

    Add login and register page only

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import logo from "../assets/logo.png"
 import Search from './Search'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { FaRegCircleUser } from "react-icons/fa6";
 import useMobile from '../hooks/useMobile';
+import { BsCart4 } from "react-icons/bs";
 
 const Header = () => {
     const [isMobile] = useMobile();
     const location = useLocation();
     const isSearchPage = location.pathname === '/search';
+    const navigate = useNavigate();
+
+    const redirectToLoginPage = () => {
+        navigate('/login')
+    }
 
     return (
-        <header className='h-24 lg:h-20 lg:shadow-md sticky top-0 flex items-center flex-col justify-center gap-1'>
+        <header className='h-24 lg:h-20 lg:shadow-md sticky top-0 flex items-center flex-col justify-center gap-1 bg-white'>
             {
                 !(isSearchPage && isMobile) && (
                     <div className='container mx-auto flex items-center px-2 justify-between'>
@@ -40,11 +46,22 @@ const Header = () => {
 
                         {/* login and my card  */}
                         <div className=''>
+                            {/* user icons display in only mobile version  */}
                             <button className='text-neutral-600 lg:hidden'>
                                 <FaRegCircleUser size={26} />
                             </button>
-                            <div className='hidden lg:block'>
-                                login and my card
+                            {/* user icons display in only Desktop version  */}
+                            <div className='hidden lg:flex items-center gap-10'>
+                                <button onClick={redirectToLoginPage} className='text-lg px-2'>Login</button>
+                                <button className='flex items-center gap-2 bg-green-800 hover:bg-gray-700 px-3 py-3 rounded text-white'>
+                                    <div className='animate-bounce'>
+                                        {/* add to card icons  */}
+                                        <BsCart4 size={28} />
+                                    </div>
+                                    <div className='font-semibold'>
+                                        <p>My Cart</p>
+                                    </div>
+                                </button>
                             </div>
                         </div>
                     </div>
--- client/src/route/index.jsx
+++ client/src/route/index.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
 import SearchPage from "../pages/SearchPage";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -14,6 +16,14 @@ const router = createBrowserRouter([
       {
         path: "search",
         element: <SearchPage />
+      },
+      {
+        path: "login",
+        element: <Login />
+      },
+      {
+        path: "register",
+        element: <Register />
       }
     ]
   },
