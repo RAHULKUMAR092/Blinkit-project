60c6d6b593dcfcd95416b23b7162443f6b3062c0
RAHUL KUMAR <srrahul092@gmail.com>
2025-02-27 14:04:51 +0530

    add more field in the upload Product page

--- client/src/pages/UploadProduct.jsx
+++ client/src/pages/UploadProduct.jsx
@@ -5,6 +5,7 @@ import Loading from "../components/Loading"
 import { MdDelete } from "react-icons/md";
 import { useSelector } from 'react-redux';
 import { IoClose } from 'react-icons/io5';
+import AddFieldComponent from '../components/AddFieldComponent';
 
 
 const UploadProduct = () => {
@@ -26,6 +27,9 @@ const UploadProduct = () => {
     const [selectCategory, setSelectCategory] = useState("");
     const [selectSubCategory, setSelectSubCategory] = useState("");
     const allSubCategory = useSelector(state => state.product.allSubCategory);
+    const [opneAddField, setOpneAddField] = useState(false);
+    const [fieldName, setFieldName] = useState("");
+
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -77,6 +81,23 @@ const UploadProduct = () => {
             }
         })
     }
+    const handleAddField = async () => {
+        setData((preve) => {
+            return {
+                ...preve,
+                more_details: {
+                    ...preve.more_details,
+                    [fieldName]: ""
+                }
+            }
+        })
+        setFieldName("")
+        setOpneAddField(false)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
 
     return (
         <section>
@@ -84,9 +105,9 @@ const UploadProduct = () => {
                 <h2 className='font-semibold'>Upload Product</h2>
             </div>
             <div className='grid p-4'>
-                <form className='grid gap-4'>
+                <form className='grid gap-4' onSubmit={handleSubmit}>
                     <div className='grid gap-1'>
-                        <label htmlFor='name'>Name</label>
+                        <label htmlFor='name' className='font-medium'>Name</label>
                         <input
                             type="text"
                             placeholder='Enter Product Name'
@@ -99,7 +120,7 @@ const UploadProduct = () => {
                         />
                     </div>
                     <div className='grid gap-1'>
-                        <label htmlFor='description'>Description</label>
+                        <label htmlFor='description' className='font-medium'>Description</label>
                         <textarea
                             id='description'
                             name="description"
@@ -114,7 +135,7 @@ const UploadProduct = () => {
                         />
                     </div>
                     <div>
-                        <p>Image</p>
+                        <p className='font-medium'>Image</p>
                         <div>
                             <label htmlFor='productImage' className='bg-blue-50 h-24 border rounded flex justify-center items-center cursor-pointer'>
                                 <div className='text-center flex justify-center items-center flex-col'>
@@ -159,7 +180,7 @@ const UploadProduct = () => {
                         </div>
                     </div>
                     <div className='grid gap-1'>
-                        <label>Category</label>
+                        <label className='font-medium'>Category</label>
                         <div>
                             <select
                                 value={selectCategory}
@@ -201,7 +222,7 @@ const UploadProduct = () => {
                         </div>
                     </div>
                     <div className='grid gap-1'>
-                        <label>Sub Category</label>
+                        <label className='font-medium'>Sub Category</label>
                         <div>
                             <select
                                 value={selectSubCategory}
@@ -242,6 +263,96 @@ const UploadProduct = () => {
                             </div>
                         </div>
                     </div>
+
+                    <div className='grid gap-1'>
+                        <label htmlFor='unit' className='font-medium'>Unit</label>
+                        <input
+                            type="text"
+                            placeholder='Enter Product Unit'
+                            name="unit"
+                            id='unit'
+                            required
+                            value={data.unit}
+                            onChange={handleChange}
+                            className='bg-blue-50 p-2 outline-none border focus-within:border-primary-200 rounded'
+                        />
+                    </div>
+
+                    <div className='grid gap-1'>
+                        <label htmlFor='stock' className='font-medium'>Number of Stock</label>
+                        <input
+                            type="number"
+                            placeholder='Enter Product Stock'
+                            name="stock"
+                            id='stock'
+                            required
+                            value={data.stock}
+                            onChange={handleChange}
+                            className='bg-blue-50 p-2 outline-none border focus-within:border-primary-200 rounded'
+                        />
+                    </div>
+
+                    <div className='grid gap-1'>
+                        <label htmlFor='price' className='font-medium'>Price</label>
+                        <input
+                            type="number"
+                            placeholder='Enter Product Price'
+                            name="price"
+                            id='price'
+                            required
+                            value={data.price}
+                            onChange={handleChange}
+                            className='bg-blue-50 p-2 outline-none border focus-within:border-primary-200 rounded'
+                        />
+                    </div>
+
+                    <div className='grid gap-1'>
+                        <label htmlFor='discount' className='font-medium'>Discount</label>
+                        <input
+                            type="number"
+                            placeholder='Enter Product Discount'
+                            name="discount"
+                            id='discount'
+                            required
+                            value={data.discount}
+                            onChange={handleChange}
+                            className='bg-blue-50 p-2 outline-none border focus-within:border-primary-200 rounded'
+                        />
+                    </div>
+                    {/* add more field  */}
+
+                    {
+                        Object?.keys(data?.more_details).map((k, index) => {
+                            return (
+                                <div className='grid gap-1'>
+                                    <label htmlFor={k} className='font-medium'>{k}</label>
+                                    <input
+                                        type="text"
+                                        id={k}
+                                        required
+                                        value={data?.more_details[k]}
+                                        onChange={(e) => {
+                                            const value = e.target.value;
+                                            setData((preve) => {
+                                                return {
+                                                    ...preve,
+                                                    more_details: {
+                                                        ...preve.more_details,
+                                                        [k]: value
+                                                    }
+                                                }
+                                            })
+                                        }}
+                                        className='bg-blue-50 p-2 outline-none border focus-within:border-primary-200 rounded'
+                                    />
+                                </div>
+                            )
+                        })
+                    }
+                    <div onClick={() => setOpneAddField(true)} className='hover:bg-primary-200 bg-white py-2 px-3 w-32 text-center font-semibold border border-primary-200 hover:text-neutral-900 cursor-pointer rounded'>
+                        Add Fields...
+                    </div>
+                    <button className='bg-primary-100 hover:bg-primary-200 py-2 rounded font-semibold'>Submit</button>
                 </form>
             </div>
             {
@@ -249,6 +360,16 @@ const UploadProduct = () => {
                     <ViewImageURL
                         url={ViewImageURL}
                         close={() => setViewImageURL("")}
+                    />
+                )
+            }
+            {
+                opneAddField && (
+                    <AddFieldComponent
+                        value={fieldName}
+                        onChange={(e) => setFieldName(e.target.value)}
+                        submit={handleAddField}
+                        close={() => setOpneAddField(false)}
                     />
                 )
             }
