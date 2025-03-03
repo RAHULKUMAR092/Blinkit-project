de2c905e2037fda50a181dd9385098b3332f94dd
RAHUL KUMAR <srrahul092@gmail.com>
2025-03-03 09:56:30 +0530

    code implement home page section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,54 +10,65 @@ import { useEffect } from 'react';
 import fetchUserDetails from './utils/fetchUserDetails';
 import { setUserDetails } from './store/userSlice';
 import { useDispatch } from 'react-redux';
-import { setAllCategory, setAllSubCategory } from './store/productSlice';
+import { setAllCategory, setAllSubCategory, setLoadingCategory } from './store/productSlice';
 import Axios from './utils/Axios';
 import SummaryApi from './common/SummaryApi';
 
 
 
-
 function App() {
-
   const dispatch = useDispatch()
+  // const location = useLocation()
+
 
   const fetchUser = async () => {
     const userData = await fetchUserDetails()
     dispatch(setUserDetails(userData.data))
-
   }
+
   const fetchCategory = async () => {
     try {
+      dispatch(setLoadingCategory(true))
       const response = await Axios({
         ...SummaryApi.getCategory
       })
       const { data: responseData } = response
+
       if (responseData.success) {
-        dispatch(setAllCategory(responseData.data))
+        dispatch(setAllCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name))))
       }
     } catch (error) {
+
     } finally {
+      dispatch(setLoadingCategory(false))
     }
   }
+
   const fetchSubCategory = async () => {
     try {
       const response = await Axios({
         ...SummaryApi.getSubCategory
       })
       const { data: responseData } = response
+
       if (responseData.success) {
-        dispatch(setAllSubCategory(responseData.data))
+        dispatch(setAllSubCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name))))
       }
     } catch (error) {
+
     } finally {
     }
   }
+
+
 
   useEffect(() => {
     fetchUser()
     fetchCategory()
     fetchSubCategory()
+    // fetchCartItem()
   }, [])
+
 
   return (
     <>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,62 @@
 import React from 'react'
+import banner from "../assets/banner.jpg"
+import bannerMobile from "../assets/banner-mobile.jpg"
+import { useSelector } from 'react-redux'
+
 
 const Home = () => {
+    const loadingCategory = useSelector(state => state.product.loadingCategory)
+    const categoryData = useSelector(state => state.product.allCategory)
+
+
+
     return (
-        <div>
-            Home
-        </div>
+        <section className='bg-white'>
+            <div className='container mx-auto'>
+                <div className={`w-full h-full min-h-48 bg-blue-100 rounded ${!banner && "animate - pulse my-2"} `}>
+                    <img
+                        src={banner}
+                        alt="banner"
+                        className="w-full h-full hidden lg:block"
+                    />
+                    <img
+                        src={bannerMobile}
+                        alt="bannerMobile"
+                        className="w-full h-full lg:hidden"
+                    />
+                </div>
+            </div>
+            <div className='container mx-auto px-4 my- grid grid-cols-5 md:grid-cols-8 lg:grid-cols-10 gap-2'>
+                {
+                    loadingCategory ? (
+                        new Array(12).fill(null).map((c, index) => {
+                            return (
+                                <div className='bg-white rounded p-4 min-h-36 grid gap-2 shadow animate-pulse'>
+                                    <div className='bg-blue-100 min-h-20 rounded'></div>
+                                    <div className='bg-blue-100 h-8 rounded'></div>
+                                </div>
+                            )
+                        })
+                    ) : (
+                        categoryData.map((cat, index) => {
+                            return (
+                                <div>
+                                    <div>
+                                        <img
+                                            src={cat.image}
+                                            alt={cat.name}
+                                            className="w-full h-full object-scale-down"
+                                        />
+                                    </div>
+                                </div>
+                            )
+                        })
+
+                    )
+
+                }
+            </div>
+        </section >
     )
 }
 
