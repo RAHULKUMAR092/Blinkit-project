1ec1e13cc34b32690e6e997c2715d99e459a210a
RAHUL KUMAR <srrahul092@gmail.com>
2025-03-07 09:28:42 +0530

    update css

--- client/src/components/CardProduct.jsx
+++ client/src/components/CardProduct.jsx
@@ -6,28 +6,28 @@ import { valideURLConvert } from '../utils/valideURLConvert'
 const CardProduct = ({ data }) => {
     const url = `/product/${valideURLConvert(data.name)}-${data._id}`
     return (
-        <Link to={url} className='border p-4 grid gap-3 max-w-52 lg:min-w-52 rounded'>
-            <div className='min-h-20 max-h-32 rounded'>
+        <Link to={url} className='border py-2 lg:p-4 grid gap-1 lg:gap-3 min-w-36 lg:min-w-52 rounded'>
+            <div className='min-h-20 w-full max-h-24 lg:max-h-32 rounded overflow-hidden'>
                 <img
                     src={data.image[0]}
-                    className='w-full h-full object-scale-down scale-125'
+                    className='w-full h-full object-scale-down lg:scale-125'
                 />
             </div>
-            <div className='rounded text-sm w-fit p-[1px] px-2 text-green-600 bg-green-50'>
+            <div className='rounded text-xs w-fit p-[1px] px-2 text-green-600 bg-green-50'>
                 10min
             </div>
-            <div className='font-medium text-ellipsis line-clamp-2'>
+            <div className='px-2 lg:px-0 font-medium text-ellipsis text-sm lg:text-base line-clamp-2'>
                 {data.name}
             </div>
-            <div className='w-fit'>
+            <div className='w-fit px-2 lg:px-0 text-sm lg:text-base'>
                 {data.unit}
             </div>
-            <div className='flex items-center justify-between gap-3'>
+            <div className='px-2 lg:px-0 flex items-center justify-between gap-1 lg:gap-3 text-sm lg:text-base'>
                 <div className='font-semibold'>
                     {DisplayPriceInRupees(data.price)}
                 </div>
                 <div className=''>
-                    <button className='bg-green-600 hover:bg-green-700 text-white px-4 py-1 rounded'>Add</button>
+                    <button className='bg-green-600 hover:bg-green-700 text-white px-2 lg:px-4 py-1 rounded'>Add</button>
                 </div>
             </div>
 
--- client/src/components/CategoryWiseProductDisplay.jsx
+++ client/src/components/CategoryWiseProductDisplay.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 import AxiosToastError from '../utils/AxiosToastError'
 import Axios from '../utils/Axios'
@@ -10,6 +10,7 @@ import { FaAngleLeft, FaAngleRight } from 'react-icons/fa'
 const CategoryWiseProductDisplay = ({ id, name }) => {
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(false)
+    const containerRef = useRef()
 
     const fetchCategoryWiseProduct = async () => {
         try {
@@ -35,6 +36,12 @@ const CategoryWiseProductDisplay = ({ id, name }) => {
     useEffect(() => {
         fetchCategoryWiseProduct()
     }, [])
+    const handleScrollRight = () => {
+        containerRef.current.scrollLeft += 200
+    }
+    const handleScrollLeft = () => {
+        containerRef.current.scrollLeft -= 200
+    }
 
     const loadingCardNumber = new Array(6).fill(null)
 
@@ -44,30 +51,33 @@ const CategoryWiseProductDisplay = ({ id, name }) => {
                 <h3 className='font-semibold text-lg md:text-xl'>{name}</h3>
                 <Link to="" className='text-green-600 hover:text-green-400'>See All</Link>
             </div>
-            <div className='flex items-center gap-4 md:gap-6 lg:gap-8 container mx-auto px-4 overflow-hidden'>
-                {
-                    loading &&
-                    loadingCardNumber.map((_, index) => {
-                        return (
-                            <CardLoading key={"CategorywiseProductDisplay123" + index} />
-                        )
-                    })
-                }
-                {
-                    data.map((p, index) => {
-                        return (
-                            <CardProduct
-                                data={p}
-                                key={p._id + "CategorywiseProductDisplay" + index}
-                            />
-                        )
-                    })
-                }
+            <div className='relative flex items-center'>
+                <div className='flex gap-4 md:gap-6 lg:gap-8 container mx-auto px-4 overflow-x-scroll scrollbar-none scroll-smooth' ref={containerRef}>
+                    {
+                        loading &&
+                        loadingCardNumber.map((_, index) => {
+                            return (
+                                <CardLoading key={"CategorywiseProductDisplay123" + index} />
+                            )
+                        })
+                    }
+                    {
+                        data.map((p, index) => {
+                            return (
+                                <CardProduct
+                                    data={p}
+                                    key={p._id + "CategorywiseProductDisplay" + index}
+                                />
+                            )
+                        })
+                    }
+
+                </div>
                 <div className='w-full left-0 right-0 container mx-auto px-2 absolute hidden lg:flex justify-between'>
-                    <button className='z-10 relative bg-white hover:bg-gray-100 shadow-lg p-2 rounded-full text-lg'>
+                    <button onClick={handleScrollLeft} className='z-10 relative bg-white hover:bg-gray-100 shadow-lg p-2 rounded-full text-lg'>
                         <FaAngleLeft />
                     </button>
-                    <button className='z-10 relative bg-white hover:bg-gray-100 shadow-lg p-2 rounded-full text-lg'>
+                    <button onClick={handleScrollRight} className='z-10 relative bg-white hover:bg-gray-100 shadow-lg p-2 rounded-full text-lg'>
                         <FaAngleRight />
                     </button>
                 </div>
